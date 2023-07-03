6da37bce7c68ac33d05986c88ca57d04c06115da
amnik-open <amnik.open@gmail.com>
2023-07-03 11:07:49 +0330

    book: add serializers for models of book app

---
+++ book/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from contacts.models import Contact, Number
+
+
+class NumberSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Number
+        fields = ['number']
+
+
+class ContactSerializer(serializers.ModelSerializer):
+    numbers = NumberSerializer(many=True)
+    class Meta:
+        model = Contact
+        fields = ['id', 'name', 'description', 'numbers']
+
+    def create(self, validated_data):
+        return Contact.objects.create(
+            name=validated_data['name'],
+            description=validated_data['description'],
+            owner=validated_data['owner'],
+            numbers=validated_data['numbers']
+        )
+
+    def update(self, instance, validated_data):
+        numbers = []
+        for n in validated_data.pop('numbers'):
+            numbers.append(n['number'])
+        name = validated_data.get('name', instance.name)
+        description = validated_data.get('description', instance.description)
+        return Contact.objects.update_contact(instance, name, description,
+                                              numbers)
