6cfcb299be87fca0b2945738833f4e1e0e02dd44
amnik-open <amnik.open@gmail.com>
2023-07-03 09:31:24 +0330

    contactbook: add postgress settings

--- contactbook/contactbook/settings.py
+++ contactbook/contactbook/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-%&66g+tgj=(o$2!&o=^l^973ik*h8mt**xi91i0fyx&v%gq#0@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'contactbook.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
