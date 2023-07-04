b8a65e420eb5a209b1631c4041bf786359b00a96
amnik-open <amnik.open@gmail.com>
2023-07-04 11:35:01 +0330

    contactbook: add accounts urls for users app

--- contactbook/urls.py
+++ contactbook/urls.py
@@ -16,13 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework.authtoken import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', include('book.urls')),
-]
-
-urlpatterns += [
-    path('api-token-auth/', views.obtain_auth_token)
+    path('accounts/', include('users.urls')),
 ]
