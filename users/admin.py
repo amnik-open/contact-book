37b5e090351e63e727620e8e6518abaec890df2a
amnik-open <amnik.open@gmail.com>
2023-07-04 11:38:49 +0330

    users: add admin form for MyUser model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .forms import MyUserCreationForm, MyUserChangeForm
+from .models import MyUser
 
-# Register your models here.
+
+class MyUserAdmin(UserAdmin):
+    add_form = MyUserCreationForm
+    form = MyUserChangeForm
+    model = MyUser
+    list_display = ("email", "is_admin", "is_active",)
+    list_filter = ("email", "is_admin", "is_active",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_admin", "is_active", "groups", "user_permissions")}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": (
+                "email", "password", "is_admin",
+                "is_active", "groups", "user_permissions"
+            )}
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+
+
+admin.site.register(MyUser, MyUserAdmin)
